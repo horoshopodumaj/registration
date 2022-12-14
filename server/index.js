ce97c74618e3d13335243db292ad48779279975d
Diana Murtazina <murka8831@yandex.ru>
2022-12-14 21:07:35 +0400

    connecting to the database

--- server/index.js
+++ server/index.js
@@ -2,12 +2,22 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
 
 const PORT = process.env.PORT || 7000;
 const app = express();
 
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
 const start = async () => {
     try {
+        await mongoose.connect(process.env.DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         app.listen(PORT, () => {
             console.log(`Server started on PORT ${PORT}`);
         });
