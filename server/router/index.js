d7b653efcee3b27a79463d9a76b15b51e19703d5
Diana Murtazina <murka8831@yandex.ru>
2022-12-14 21:31:21 +0400

    added controllers

--- server/router/index.js
+++ server/router/index.js
@@ -1,11 +1,12 @@
 const Router = require("express").Router;
 const router = new Router();
+const userController = require("../controllers/userController");
 
-router.post("/registration");
-router.post("/login");
-router.post("/logout");
-router.get("/activate/:link");
-router.get("/refresh");
-router.get("/test");
+router.post("/registration", userController.registration);
+router.post("/login", userController.login);
+router.post("/logout", userController.logout);
+router.get("/activate/:link", userController.activate);
+router.get("/refresh", userController.refresh);
+router.get("/test", userController.test);
 
 module.exports = router;
