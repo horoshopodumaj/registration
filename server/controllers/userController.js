653581153b362d1937f2101484d3cf39e73ead0a
Diana Murtazina <murka8831@yandex.ru>
2022-12-15 14:58:34 +0400

    validation of the request body

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,14 @@
 const userService = require("../service/userService");
+const { validationResult } = require("express-validator");
+const ApiError = require("../exceptions/apiError");
 
 class UserController {
     async registration(req, res, next) {
         try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return next(ApiError.BadRequest("Ошибка при валидации", errors.array()));
+            }
             const { email, password } = req.body;
             const userData = await userService.registration(email, password);
             res.cookie("refreshToken", userData.refreshToken, {
